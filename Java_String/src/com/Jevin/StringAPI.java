b251fbfb9bd221effaeb7d702a353643a4125147
Jevin Zhao <JevinZhao@gmail.com>
2021-05-03 11:15:39 +0800

    create project

---
+++ Java_String/src/com/Jevin/StringAPI.java
@@ -0,0 +1,18 @@
+package com.Jevin;
+
+/**
+ * 总结的一些String API使用方法
+ */
+public class StringAPI {
+    /**
+     * 测试主入口
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+    /**
+     * 测试方法
+     */
+
+}
